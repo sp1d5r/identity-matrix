8b473f0f468f5e1d9b67fc5f8f758c0243350387
Elijah <aa1719@imperial.ac.uk>
2023-05-14 11:22:13 +0100

    Frontend + Backend: Initial boilerplate setup firebase + REST API.

---
+++ README.md
@@ -0,0 +1,54 @@
+<h1 align="center">Identity Matrix</h1>
+
+<p align="center">
+  <img src="path_to_your_logo_icon" alt="Logo" width="200" height="200">
+</p>
+
+<p align="center">
+  <strong>Facial Recognition REST API</strong>
+</p>
+
+<p align="center">
+  <a href="LICENSE"><img src="https://img.shields.io/badge/license-MIT-blue.svg" alt="License"></a>
+  <a href="https://github.com/sp1d5r/identity_matrix/issues"><img src="https://img.shields.io/github/issues/your_username/your_repository.svg" alt="GitHub Issues"></a>
+  <a href="https://github.com/sp1d5r/identity_matrix/stargazers"><img src="https://img.shields.io/github/stars/your_username/your_repository.svg" alt="GitHub Stars"></a>
+  <a href="https://github.com/sp1d5r/identity_matrix/network/members"><img src="https://img.shields.io/github/forks/your_username/your_repository.svg" alt="GitHub Forks"></a>
+</p>
+
+## Description
+
+Essentially the goal of this is to deploy a restful API to google cloud functions for my deepfake app. Seems like there 
+is a lot of money to be made in deepfake apps since I keep getting advertisements for them. Making something like this 
+but also scalable could be a fun project.
+
+## Demo
+
+TBC
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [API Documentation](#api-documentation)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+`pip install requirements.txt`
+
+## Usage
+
+TBC
+
+## API Documentation
+
+TBC
+
+## Contributing
+
+If you would like to contribute to this project, please follow the guidelines outlined in the [CONTRIBUTING.md](CONTRIBUTING.md) file.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, jsonify
+import firebase_admin
+from firebase_admin import credentials, storage
+from werkzeug.utils import secure_filename
+
+# Flask initialisation
+app = Flask(__name__)
+
+# Initialize Firebase
+cred = credentials.Certificate('path/to/serviceAccountKey.json')
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'your-bucket-name.appspot.com'
+})
+
+#
+# ROUTES
+#
+
+
+def upload_file(file, folder):
+    filename = secure_filename(file.filename)
+    blob = storage.bucket().blob(f'{folder}/{filename}')
+    blob.upload_from_string(
+        file.read(),
+        content_type=file.content_type
+    )
+    return filename
+
+@app.route('/api/upload/video/', methods=['POST'])
+def upload_video():
+    if 'video' not in request.files:
+        return jsonify({'error': 'no file'}), 400
+    video = request.files['video']
+    filename = upload_file(video, 'videos')
+    return jsonify({'filename': filename}), 200
+
+@app.route('/api/upload/img/', methods=['POST'])
+def upload_img():
+    if 'image' not in request.files:
+        return jsonify({'error': 'no file'}), 400
+    image = request.files['image']
+    filename = upload_file(image, 'images')
+    return jsonify({'filename': filename}), 200
+
+@app.route('/api/process', methods=['POST'])
+def process():
+    data = request.get_json()
+    if 'video_filename' not in data or 'image_filename' not in data:
+        return jsonify({'error': 'missing filename'}), 400
+    video_filename = data['video_filename']
+    image_filename = data['image_filename']
+    # The actual processing would likely be done in a background task,
+    # but for simplicity, we'll assume it's done here.
+    result_filename = process_video(video_filename, image_filename)
+    return jsonify({'result_filename': result_filename}), 200
+
+def process_video(video_filename, image_filename):
+    # This is where you would implement your processing logic.
+    # For now, we'll just return the video filename.
+    return video_filename
+
+
+# Run the Flask App
+if __name__ == '__main__':
+    app.run()
