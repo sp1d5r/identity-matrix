8f74f5159636e1acd2e946d491e35ddc30232c62
Elijah <aa1719@imperial.ac.uk>
2023-09-24 17:47:12 +0100

    Backend: Added download and video reconsilation bits.

--- app/routes/download.py
+++ app/routes/download.py
@@ -1,15 +1,52 @@
+import json
+
+from firebase_admin import storage
 from flask import Blueprint, request, jsonify
+import datetime
+
+
+from app.services import firebase_service
 
 bp = Blueprint('download', __name__, url_prefix='/api')
 
-
 @bp.route('/download', methods=['GET'])
 def status():
-    job_id = request.args.get('job_id')
-    if job_id is None:
-        return jsonify({'error': 'job_id parameter is missing'}), 400
+    data = request.get_json()
 
-    # TODO: Get a download of the job id
+    # Validate job_id in the request
+    job_id = data.get('job_id')
+    if not job_id:
+        return jsonify({'error': 'Job ID is required'}), 400
 
-    # Example response with the job_id
-    return jsonify({'downloaded_file_job_id': job_id}), 200
+    # Retrieve the job information
+    job_information, job_status_code = firebase_service.get_job(job_id)
+    if job_status_code != 200:
+        return job_information, job_status_code
+
+
+    job_data = json.loads(job_information.get_data(as_text=True))
+    video_name = job_data.get('video')
+
+    print(video_name)
+
+    # Construct the path to the file in Firebase Storage using the job_id
+    # You might need to modify this depending on your actual path structure
+    file_path_in_bucket = f'output/{video_name}'
+    # Get the bucket
+    bucket = storage.bucket(name='face-flipper.appspot.com')
+
+    # Get the blob
+    blob = bucket.blob(file_path_in_bucket)
+
+    # Check if blob exists
+    if not blob.exists():
+        return jsonify({'error': 'File not found'}), 404
+
+    # Define the expiration time for the signed URL
+    expiration_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=15)  # URL will be valid for 15 minutes
+
+    # Generate a download URL
+    download_url = blob.generate_signed_url(expiration=expiration_time)
+
+    # Return a JSON response with the download URL
+    return jsonify({'download_url': download_url}), 200
--- app/routes/process.py
+++ app/routes/process.py
@@ -5,6 +5,10 @@ import os
 import cv2
 from firebase_admin import storage, firestore
 from app.services import firebase_service
+import tempfile
+from werkzeug.utils import secure_filename
+import shutil
+from moviepy.editor import ImageSequenceClip, AudioFileClip
 from app.services.face_swapping import swap_faces
 
 bp = Blueprint('process', __name__, url_prefix='/api')
@@ -109,3 +113,88 @@ def swap_frame():
         firebase_service.update_progress(job_id, frame=frame_count, progress=progress, stage="FACE_SWAPPING")
 
     return jsonify({'result_filename': "Success..."}), 200
+
+
+@bp.route('/reconsile-video/', methods=['POST'])
+def reconcile_video():
+    data = request.get_json()
+
+    # Validate job_id in the request
+    job_id = data.get('job_id')
+    if not job_id:
+        return jsonify({'error': 'Job ID is required'}), 400
+
+    # Retrieve the job information
+    job_information, job_status_code = firebase_service.get_job(job_id)
+    if job_status_code != 200:
+        return job_information, job_status_code
+
+    firebase_service.update_progress(job_id, progress=100, stage="CREATING_VIDEO")
+
+    job_data = json.loads(job_information.get_data(as_text=True))
+    video_name = job_data.get('video')
+
+    swapped_frames_prefix = f"swapped_frames/{video_name}/frame_"
+
+    # Create a temporary directory to store the downloaded frames
+    temp_dir = tempfile.mkdtemp()
+
+    # Download the original video
+    input_video_blob = bucket.blob(f"videos/{video_name}")
+    input_video_path = os.path.join(temp_dir, video_name)
+    input_video_blob.download_to_filename(input_video_path)
+
+    # Get the list of blobs (frames) in Firebase Storage
+    blobs = bucket.list_blobs(prefix=swapped_frames_prefix)
+    frames = []
+    for blob in blobs:
+        print("Downloading Swapped Frame: " + blob.name)
+        frame_path = os.path.join(temp_dir, os.path.basename(blob.name))
+        blob.download_to_filename(frame_path)
+        frames.append(frame_path)
+
+    # Sort frames by number
+    frames.sort(key=lambda x: int(x.split('_')[-1].split('.')[0]))
+
+    # Get the frame rate of the original video
+    from moviepy.editor import VideoFileClip
+
+    original_clip = VideoFileClip(input_video_path)
+    frame_rate = original_clip.fps
+    original_audio = original_clip.audio
+
+    # Create a new video clip from the swapped frames
+    new_clip = ImageSequenceClip(frames, fps=frame_rate)
+
+    # Set the audio of the new clip to be the same as the original audio
+    new_clip = new_clip.set_audio(original_audio)
+
+    # Write the result to a file
+    output_video_path = os.path.join(temp_dir, "output_" + video_name)
+    new_clip.write_videofile(output_video_path, codec='libx264')
+
+    # Upload the reconciled video to Firebase Storage
+    upload_reconciled_video(output_video_path, "output", video_name)
+
+    # Clean up temporary directory
+    shutil.rmtree(temp_dir)
+
+    firebase_service.update_progress(job_id, progress=100, stage="FINISHED")
+
+    return jsonify({'success': 'Video Reconstructed!'}), 200
+
+
+def upload_reconciled_video(file_path, folder, original_filename):
+    # Secure the original filename
+    secure_original_filename = secure_filename(original_filename)
+
+    # Create a blob in the specified folder with the original filename
+    blob = bucket.blob(f'{folder}/{secure_original_filename}')
+
+    # Upload the file to the blob
+    with open(file_path, 'rb') as file:
+        blob.upload_from_file(
+            file,
+            content_type="video/mp4"
+        )
+    print(f"Uploaded {file_path} to {folder}/{secure_original_filename}")
